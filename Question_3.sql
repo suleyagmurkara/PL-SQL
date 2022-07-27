046d271f21058e6723273d2211cf4d86d2a86968
suleyagmurkara <suleyagmurkara@gmail.com>
2022-07-27 10:38:42 +0300

    Update Question_3.sql

--- Question_3.sql
+++ Question_3.sql
@@ -13,21 +13,21 @@ FROM   hr.departments dept, v_minus
 WHERE  dept.department_id = v_minus.deptid
 
 ------------------------------------------------------
--- DEPARTMENT_ID   DEPARTMENT_NAME       5th letter
--- 120	         Treasury              s
--- 130	         Corporate Tax         o
--- 140	         Control And Credit    r
--- 150	         Shareholder Services  e
--- 160	         Benefits              f
--- 170	         Manufacturing         f
--- 180	         Construction          t
--- 190	         Contracting           r
--- 200	         Operations            a
--- 210	         IT Support            u
--- 220	         NOC                   - 
--- 230	         IT Helpdesk           e
--- 240	         Government Sales      r
--- 250	         Retail Sales          i
--- 260	         Recruiting            u
--- 270	         Payroll               o
+-- DEPARTMENT_ID  DEPARTMENT_NAME        5th letter
+-- 120	         Treasury                 s
+-- 130	         Corporate Tax            o
+-- 140	         Control And Credit       r
+-- 150	         Shareholder Services     e
+-- 160	         Benefits                 f
+-- 170	         Manufacturing            f
+-- 180	         Construction             t
+-- 190	         Contracting              r
+-- 200	         Operations               a
+-- 210	         IT Support               u
+-- 220	         NOC                      - 
+-- 230	         IT Helpdesk              e
+-- 240	         Government Sales         r
+-- 250	         Retail Sales             i
+-- 260	         Recruiting               u
+-- 270	         Payroll                  o
 ------------------------------------------------------
